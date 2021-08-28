b311924516cf4a0d275cd79da98be3a5bb31d035
paucabral <josepaucabral@gmail.com>
2021-08-28 18:24:51 +0800

    Added password reset functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,12 @@ urlpatterns = [
     path('registration-success/', views.RegistrationSuccess.as_view(),
          name='registration-success'),
     path('logout/', views.logoutUser, name='logout'),
+    path('reset-password/', auth_views.PasswordResetView.as_view(
+        template_name="accounts/reset-password.html"), name="reset_password"),
+    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/reset-password-sent.html"), name="password_reset_done"),
+    path('reset-password-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
+        template_name="accounts/reset-password-confirm.html"), name="password_reset_confirm"),
+    path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="accounts/reset-password-complete.html"), name="password_reset_complete"),
 ]
