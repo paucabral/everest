5a4de9b85931447650c90e94760d61f3b27faee8
paucabral <josepaucabral@gmail.com>
2021-08-29 21:13:37 +0800

    updated allowed host to refer on environement variable

--- event_registration_system/settings.py
+++ event_registration_system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('DOMAIN_NAME')]
 
 
 # Application definition
